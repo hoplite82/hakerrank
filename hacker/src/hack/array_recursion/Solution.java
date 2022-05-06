a8e472130bec4ccce0ea351140c28fe030e2e527
robert pratersch <robert@pratersch.de>
2022-05-06 16:43:10 +0200

    first try

--- hacker/src/hack/array_recursion/Solution.java
+++ hacker/src/hack/array_recursion/Solution.java
@@ -14,89 +14,40 @@ public class Solution {
 		if (game[0] != 0)
 			return false;
 		else
-			return canWin(leap, game, 0, true, null);
-//		int n = game.length - 1;
-//		int i = n-1;		
-//
-//		Deque<Integer> winPoses;
-//		do {
-//			winPoses = findAllWinPos(leap, game, i);
-//			
-//			
-//			
-//		} while (!winPoses.isEmpty());
-//		return false;
+			return canWin(leap, game, 0);
 
 	}
 
-	static Deque<Integer> findAllWinPos(int leap, int[] game, int i) {
-		Deque<Integer> winPoses = new ArrayDeque<>();
-		int j = i;
-		while (game[j--] == 0)
-			leap++;
-		while (leap >= 0) {
-			if (game[i - leap] == 0)
-				winPoses.add(i - leap);
-			leap--;
-		}
-		return winPoses;
-	}
 
-	public static boolean canWin(int leap, int[] game, int i, boolean forward, Deque<Integer> leapposes) {
+	public static boolean canWin(int leap, int[] game, int i) {
 		int n = game.length;
-
 		if (i + 1 >= n || (i + leap) >= n) {
 			return true;
-		} else if (game[i - 1] == 0) {
-			return canWin(leap, game, i - 1, false);
-		}
-			
-			
-			
-			leapposes = new ArrayDeque<>();
-			if (game[i + 1] == 0 && forward) {
-				if (game[i + leap] == 0)
-					leapposes.push(i);
-			} else if (game[i + leap] == 0) {
-				return canWin(leap, game, i + leap, true, leapposes);
-			}  else if (!leapposes.isEmpty()) {
-				i = leapposes.pop();
-				return canWin(leap, game, i + leap, true, leapposes);
-			} else {
-			
-			} else if(i - 1 < 0) {
-				return false;
+		} else {
+			int j = 0;
+			while (game[i - 1] == 0 && (leap - j++) >= 0) {
+				if (canWin(leap, game, i))
+					return true;
+				else
+					i--;
+//					game[i] = -1;
+
+			}
+			i = i + j;
+			j = 0;
+			while (game[i] == 0 && (leap - j++) >= 0) {
+				if (canWin(leap, game, i))
+					return true;
+				else
+					i--;
 			}
 		}
 		return false;
-
-//				
-//
-//				for (int j = 1; i + j <= i + leap; j++) {
-//					if (game[j + i] == 0)
-//						stack.push(j+1);
-//						next.add(j + i);
-//				}
-//				if (!next.isEmpty()) {
-//					while(!next.isEmpty()) {
-//						int nextpos = next.pollLast();
-//						int nextinstack = next.pop();
-//						
-//						
-//					}
-////					if (game[i - 1] == 0) {
-////						return canWin(leap, game, i - 1, false);
-////					} else {
-////						// calc jumps
-////						return false;
-////					}
-
 	}
 
-	}
 
 	public static void main(String[] args) throws IOException {
-		Scanner scan = new Scanner(new FileInputStream(new File("testdata/hack_arraygame/inunit.txt")));
+		Scanner scan = new Scanner(new FileInputStream(new File("testdata/hack_arraygame/input02.txt")));
 		int q = scan.nextInt();
 		while (q-- > 0) {
 			int n = scan.nextInt();
